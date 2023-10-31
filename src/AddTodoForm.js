aeb7d9858be781a6623b360b84344ab55fabca4d
Doug <douggyharmon@gmail.com>
2023-10-31 19:46:45 -0400

    todo items can now be added w/ UI

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,19 +1,28 @@
 import React from 'react';
 
-function AddTodoForm(props) {
+function AddTodoForm({addTodo, todoTitle, setTodoTitle}) {
+
+    const handleTitleChange = (event) => {
+        const newTodoTitle = event.target.value
+        setTodoTitle(newTodoTitle)
+    }
 
     const handleAddTodo = (event) => {
         event.preventDefault()
-        const todoTitle = event.target.title.value
-        props.onAddTodo(todoTitle)
-        event.target.title.value = ''
+        addTodo({ title: todoTitle, id: Date.now() })
+        setTodoTitle("")
     }
-
 
     return (
         <form onSubmit={handleAddTodo}>
             <label htmlFor='todoTitle'>Title</label>
-            <input type='text' id='todoTitle' name='title'></input>
+            <input
+                type='text'
+                id='todoTitle'
+                name='title'
+                value={todoTitle}
+                onChange={handleTitleChange}
+            />
             <button type='submit'>Add</button>
         </form>
     )
